c52cc8cac4356e2a68775031c66f7a3357196130
Rahul <rahulpraj4121@gmail.com>
2020-08-23 23:35:04 -0700

    travello

---
+++ mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+def home(request):
+    
+
+    
+
+    dests = Destination.objects.all()
+
+    return render(request,"index.html",{'dests':dests})
+
+
+
+
